f0d90883bfc5b1ed8a080a4fd7279c72b8a45d07
Soltanov Nijat <NijatjS@code.local>
2024-01-21 17:38:35 +0400

    first c# procekt

---
+++ Week2.app1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Week2.app1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float firstNumber;
+            float secondNumber;
+            Console.WriteLine("Please enter first number");
+            firstNumber= float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter second number");
+            secondNumber = float.Parse(Console.ReadLine());
+
+            
+
+            float resaultSum = firstNumber + secondNumber;
+
+            Console.WriteLine("adition result :" +resaultSum);
+
+            float resaultMultipi = firstNumber * secondNumber;
+
+            Console.WriteLine("Multipi " + resaultMultipi);
+
+            float resaultMinus = firstNumber - secondNumber;
+
+            Console.WriteLine("Minus " + resaultMinus);
+
+            float resaultDivide = firstNumber / secondNumber;
+
+            Console.WriteLine("Divide " + resaultDivide);
+
+
+
+
+        }
+    }
+}
